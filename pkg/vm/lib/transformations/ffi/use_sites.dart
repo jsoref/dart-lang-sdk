b00f99cbd786a72139028792de9c8fbfb5ed43d9
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-12-12 23:13:42 -0500

    spelling: mem copy
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- pkg/vm/lib/transformations/ffi/use_sites.dart
+++ pkg/vm/lib/transformations/ffi/use_sites.dart
@@ -524,7 +524,7 @@ mixin _FfiUseSiteTransformer on FfiTransformer {
     return ConstructorInvocation(constructor, Arguments([pointer]));
   }
 
-  /// Replaces a `.ref=` or `[]=` on a compound pointer extension with a memcopy
+  /// Replaces a `.ref=` or `[]=` on a compound pointer extension with a mem copy
   /// call.
   Expression _replaceSetRef(StaticInvocation node) {
     final target = node.arguments.positional[0]; // Receiver of extension
